bbb36c221dd354ae4615c6b50162687ebd710365
HuangJian <1342042894@qq.com>
2021-10-29 18:02:23 +0800

    update

---
+++ StudyBook_LearningRuby/p013_w.rb
@@ -0,0 +1,8 @@
+names1 = [ 'ann', 'richard', 'william', 'susan', 'pat' ]
+puts names1[0]
+puts names1[3]
+
+# This is the same
+names2 = %w{ ann richard william susan pat }
+puts names2[0]
+puts names2[3]
---
+++ StudyBook_LearningRuby/p013expint.rb
@@ -0,0 +1,14 @@
+# p013expint.rb
+
+def say_goodnight(name)
+  result = "Good night, #{name}"
+  return result
+end
+puts say_goodnight('Satish')
+
+
+def say_goodnight2(name)
+  "Good night, #{name}"
+end
+puts say_goodnight2('Talim')
+
